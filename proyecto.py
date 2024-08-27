83739150f6a48b2c3b834a3bbe96c70cdfeae080
unknown <fcabreraclaros@gmail.com>
2024-08-27 18:14:17 -0400

    limpia

--- proyecto.py
+++ proyecto.py
@@ -4,6 +4,8 @@ import os
 import tensorflow as tf
 import cv2
 import imghdr
+import numpy as np
+from matplotlib import pyplot as plt
 
 # 2 - Limpiar imágenes:
 
@@ -25,21 +27,7 @@ for image_class in os.listdir(data_dir):
 
 # 3 - Cargar datos:
 
-import numpy as np
-from matplotlib import pyplot as plt
-
 data = tf.keras.utils.image_dataset_from_directory('data')
-# data = tf.keras.utils.image_dataset_from_directory(
-#     'data',
-#     labels='inferred',
-#     label_mode='categorical',
-#     class_names=None,
-#     color_mode='rgb',
-#     batch_size=32,
-#     image_size=(256, 256),
-#     shuffle=True,
-#     seed=None,
-# )
 data_iterator = data.as_numpy_iterator()
 batch = data_iterator.next()
 
@@ -56,9 +44,9 @@ data.as_numpy_iterator().next()
 
 # 5 - Chunking Datos:
 
-train_size = int(len(data)*.7)
-val_size = int(len(data)*.2)
-test_size = int(len(data)*.1)
+train_size = int(len(data) * .7)
+val_size = int(len(data) * .2)
+test_size = int(len(data) * .1)
 
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
@@ -67,7 +55,7 @@ test = data.skip(train_size+val_size).take(test_size)
 # 6 - Construcción del Modelo:
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
 num_classes = 5
 model = Sequential()
@@ -80,12 +68,8 @@ model.add(Conv2D(16, (3,3), 1, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
-# model.add(Dense(1, activation='sigmoid')) # original binario
-# model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy']) # original binario
 model.add(Dense(num_classes, activation='softmax'))
-# model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.compile(optimizer='adam', loss=tf.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
-
 
 model.summary()
 
@@ -95,38 +79,25 @@ logdir='logs'
 
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+hist = model.fit(train, epochs=21, validation_data=val, callbacks=[tensorboard_callback])
 
 # 8 - Gráficos de Rendimiento
 
-fig = plt.figure()
-plt.plot(hist.history['loss'], color='teal', label='perdida')
-plt.plot(hist.history['val_loss'], color='orange', label='val_perdida')
-fig.suptitle('Pérdida', fontsize=20)
-plt.legend(loc="upper left")
-plt.show()
+# fig = plt.figure()
+# plt.plot(hist.history['loss'], color='teal', label='perdida')
+# plt.plot(hist.history['val_loss'], color='orange', label='val_perdida')
+# fig.suptitle('Pérdida', fontsize=20)
+# plt.legend(loc="upper left")
+# plt.show()
 
-fig = plt.figure()
-plt.plot(hist.history['accuracy'], color='teal', label='exactitud')
-plt.plot(hist.history['val_accuracy'], color='orange', label='val_exactitud')
-fig.suptitle('Exactitud', fontsize=20)
-plt.legend(loc="upper left")
-plt.show()
+# fig = plt.figure()
+# plt.plot(hist.history['accuracy'], color='teal', label='exactitud')
+# plt.plot(hist.history['val_accuracy'], color='orange', label='val_exactitud')
+# fig.suptitle('Exactitud', fontsize=20)
+# plt.legend(loc="upper left")
+# plt.show()
 
 # 9 - Evaluación
-
-# from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
-
-# pre = Precision()
-# re = Recall()
-# acc = BinaryAccuracy()
-
-# for batch in test.as_numpy_iterator():
-#     X, y = batch
-#     yhat = model.predict(X)
-#     pre.update_state(y, yhat)
-#     re.update_state(y, yhat)
-#     acc.update_state(y, yhat)
 
 from tensorflow.keras.metrics import CategoricalAccuracy
 acc = CategoricalAccuracy()
@@ -146,13 +117,8 @@ plt.show()
 
 yhat = model.predict(np.expand_dims(resize/255, 0))
 
-# if yhat > 0.5:
-#     print(f'La clase es: Reloj')
-# else:
-#     print(f'La clase es: Café')
-
 predicted_class = np.argmax(yhat, axis=1)
-class_names = ['diez', 'veinte', 'cincuenta', 'cien', 'doscientos']  # Replace with your actual class names
+class_names = ['cien', 'cincuenta', 'diez', 'doscientos', 'veinte']  # Replace with your actual class names
 print(class_names)
 print(f'La clase es: {class_names[predicted_class[0]]}')
 
